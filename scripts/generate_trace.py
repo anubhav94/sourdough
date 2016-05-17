c29c112cbba50d2ae854ecc3dc8c1cf32cc5193b
Anubhav Jain <anubhav94@gmail.com>
2016-05-17 00:38:15 -0400

    Added sine wave trace generator

--- scripts/generate_trace.py
+++ scripts/generate_trace.py
@@ -1,5 +1,7 @@
 import os
-def generate_trace():
+import math
+import random
+def generate_linear_trace():
 	with open('../../mahimahi/traces/trace_linear.up', 'w') as f:
 		with open('../../mahimahi/traces/trace_linear.down', 'w') as f2:
 			for i in xrange(140000):
@@ -9,4 +11,20 @@ def generate_trace():
 	f.close()
 	os.system('cp ../../mahimahi/traces/trace_linear.* /usr/local/share/mahimahi/traces/')
 
-generate_trace()
+def generate_sine_trace():
+	interval = 0.0
+	with open('../../mahimahi/traces/trace_sine.up', 'w') as f:
+		with open('../../mahimahi/traces/trace_sine.down', 'w') as f2:
+			for i in xrange(560000):
+				probability = (((math.sin(i/2000)) + 1) / 2) * 10000
+				rand_num = random.randint(1,10000)
+				if (rand_num <  probability):
+					f.write(str(i/2) + '\n')
+					f2.write(str(i/2) + '\n')
+		f2.close()
+	f.close()
+	os.system('cp ../../mahimahi/traces/trace_sine.* /usr/local/share/mahimahi/traces/')
+
+
+
+generate_sine_trace()
